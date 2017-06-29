45b61752e627d199dd94443b348d21e4076dcb9a
Gabriel Solomon <hello@gabrielsolomon.ro>
2017-06-29 13:05:13 +0300

    add gateway notification trait

---
+++ src/Message/Traits/GatewayNotificationRequestTrait.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace ByTIC\Omnipay\Common\Message\Traits;
+
+use Omnipay\Common\Message\ResponseInterface;
+
+/**
+ * Class GatewayNotificationRequestTrait
+ * @package ByTIC\Omnipay\Common\Message\Traits
+ *
+ * @property \Symfony\Component\HttpFoundation\Request $httpRequest
+ */
+trait GatewayNotificationRequestTrait
+{
+    /**
+     * Send the request
+     *
+     * @return ResponseInterface|bool
+     */
+    public function send()
+    {
+        if ($this->isProviderRequest()) {
+            return parent::send();
+        }
+
+        return false;
+    }
+
+    /**
+     * @return bool
+     */
+    protected function isProviderRequest()
+    {
+        return false;
+    }
+
+    /**
+     * @return bool
+     */
+    public function hasGet()
+    {
+        foreach (func_get_args() as $key) {
+            if (!$this->httpRequest->query->has($key)) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    /**
+     * @return bool
+     */
+    public function hasPOST()
+    {
+        foreach (func_get_args() as $key) {
+            if (!$this->httpRequest->request->has($key)) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
