fec0bfb19c5b7e221c412616849b6e9907bf7bb7
Gabriel Solomon <hello@gabrielsolomon.ro>
2020-10-05 11:32:41 +0300

    initial add soap request traits

---
+++ src/Message/Traits/Soap/AbstractSoapResponseTrait.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace ByTIC\Omnipay\Common\Message\Traits\Soap;
+
+use Omnipay\Common\Message\AbstractResponse;
+
+/**
+ * Trait AbstractSoapResponseTrait
+ * @package ByTIC\Omnipay\Common\Message\Traits\Soap
+ */
+trait AbstractSoapResponseTrait
+{
+
+}
